aa50876318ee0213c31e05d696d15cb0d9e8d519
carlsrensen <carlsrensen@gmail.com>
2023-05-25 10:21:09 +0200

    jepper

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
+// Toggle menu function
+function toggleMenu() {
+  const menu = document.querySelector('.menu');
+  menu.classList.toggle('active');
+}
 
-// Example JavaScript functionality
-document.addEventListener('DOMContentLoaded', function() {
-    // Add a click event listener to a specific element
-    var element = document.querySelector('.menu a');
-    element.addEventListener('click', function(event) {
-      // Prevent the default behavior of the link
-      event.preventDefault();
-      
-      // Perform any desired actions when the link is clicked
-      console.log('Link clicked!');
-    });
-  });
-  
+// Event listener for burger menu
+const burgerMenu = document.querySelector('.burger-menu');
+burgerMenu.addEventListener('click', toggleMenu);
