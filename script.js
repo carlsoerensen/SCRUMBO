2b349e5762f65b144968fa38bada1a8f87cbce70
carlsrensen <carlsrensen@gmail.com>
2023-05-25 12:15:37 +0200

    yo

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function toggleMenu() {
 // Event listener for burger menu
 const burgerMenu = document.querySelector('.burger-menu');
 burgerMenu.addEventListener('click', toggleMenu);
+
+function myFunction() {
+  // Get the checkbox
+  let checkBox = document.getElementById("myCheck");
+  // Get the output text
+  let text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
