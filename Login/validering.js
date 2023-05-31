0191ddfd0a5a324b42fbfdcbe0646eb3e3fcde30
carlsrensen <carlsrensen@gmail.com>
2023-05-31 14:20:55 +0200

    sdcds

--- Login/validering.js
+++ Login/validering.js
@@ -1,48 +1,23 @@
-//her validerer vi på login siden
-
-// Here we validate on the login page
-
-let email = document.forms['form']['email'];
-let password = document.forms['form']['password'];
-
-let email_error = document.getElementById('email_error');
-let pass_error = document.getElementById('pass_error');
-
-email.addEventListener('input', email_Verify);
-password.addEventListener('input', pass_Verify);
-
-function validated() {
-    if (email.value.length < 9) {
-        email.style.border = "1px solid red";
-        email_error.style.display = "block";
-        email.focus();
-        return false;
+function validatePassword(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Get the entered username and password
+    let usernameInput = document.getElementById('username');
+    let passwordInput = document.getElementById('password');
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+  
+    // Check if the username and password match the expected values
+    if (username === 'sharwarma' && password === 'Kebab') {
+      // Redirect to the forside.html page
+      window.location.href = '/undersider/forside.html';
+    } else {
+      alert('Invalid username or password. Please try again.');
+      // Clear the input fields
+      usernameInput.value = '';
+      passwordInput.value = '';
+      // Set focus to the username field
+      usernameInput.focus();
     }
-
-    if (password.value.length < 6) {
-        password.style.border = "1px solid red";
-        pass_error.style.display = "block";
-        password.focus();
-        return false;
-    }
-
-    return true; // Added return statement for successful validation
-}
-
-function email_Verify() {
-    if (email.value.length >= 8) {
-        email.style.border = "1px solid silver";
-        email_error.style.display = "none";
-    }
-
-    validated(); // Call validated() to perform overall validation
-}
-
-function pass_Verify() {
-    if (password.value.length >= 6) {
-        password.style.border = "1px solid silver";
-        pass_error.style.display = "none";
-    }
-
-    validated(); // Call validated() to perform overall validation
-}
+  }
+  
